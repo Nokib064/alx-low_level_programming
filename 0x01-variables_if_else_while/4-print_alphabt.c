f51b5c6f2c2e671f1486cf04e93e3b253ee3fa0e
Ismaeel Ekundayo <ismaelekundayo3@gmail.com>
2023-03-17 19:36:00 -0700

    prints alphabets without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+		if (alph != 'q' && alph != 'e')
+			putchar(alph);
+	putchar('\n');
+	return (0);
+}
