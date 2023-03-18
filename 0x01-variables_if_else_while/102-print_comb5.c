1758a4ade64d0736b45e35b3e230f55a0f8437aa
Ismaeel Ekundayo <ismaelekundayo3@gmail.com>
2023-03-18 14:25:32 -0700

    prints two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,16 +9,23 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 100 ; i++)
 	{
-		for (j =  0 ; j < 10 ; j++)
+		for (j =  0 ; j < 100 ; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i == 99 && j == 99)
+			if (j > i)
 			{
-				putchar(',');
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
 				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+
+				if (i * 100 + j != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
