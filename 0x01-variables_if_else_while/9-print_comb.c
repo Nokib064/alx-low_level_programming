7ac8d9fe7cd71fea576ae6826b512745f9f84193
Ismaeel Ekundayo <ismaelekundayo3@gmail.com>
2023-03-17 21:00:55 -0700

    prints number combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -main function
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		printf("%d", a);
+		printf(",");
+		printf(" ");
+	}
+	return (0);
+}
+
