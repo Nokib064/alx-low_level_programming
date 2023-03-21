766528460905bcc307e3a7f98961ba50b111f399
Ismaeel Ekundayo <ismaelekundayo3@gmail.com>
2023-03-20 18:25:47 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - main function(prints a line to a standard error
- *
- * Return: 1 
+ * main - main functiin
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return(1);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
 }
