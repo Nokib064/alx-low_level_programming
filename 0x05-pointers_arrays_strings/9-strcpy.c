23dca56a5d2a0f9aef22fc8ba7f3c506fdd4203b
Ismaeel Ekundayo <ismaelekundayo3@gmail.com>
2023-03-28 15:59:52 -0700

    copy string pointed to by array

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+ #include "main.h"
+
+/**
+ * *_strcpy - copy from pointer
+ * @dest: array
+ * @:src: another array
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (*(src + a) != '\0')
+	{
+		*(dest + a) = *(src + a);
+		a++;
+	}
+	*(dest + a) = '\0';
+	return (dest);
+}
