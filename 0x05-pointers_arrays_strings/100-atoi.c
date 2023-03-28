55ead65c67f017efff9f80d1401b00ef0fb2f35c
Ismaeel Ekundayo <ismaelekundayo3@gmail.com>
2023-03-28 16:35:29 -0700

    convert string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to int
+ * @s: pointer to be converted
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int a = 0;
+	unsigned int xy = 0;
+	int sh = 1;
+	int cd = 0;
+
+	while (s[a])
+	{
+		if (s[a] == 45)
+		{
+			sh *= -1;
+		}
+		while (s[a] >= 48 && s[a] <= 57)
+		{
+			cd = 1;
+			xy = (xy * 18) + (s[a] - '0');
+			a++;
+		}
+		if (cd == 1)
+		{
+			break;
+		}
+		a++;
+	}
+	xy *= sh;
+	return (xy);
+}
