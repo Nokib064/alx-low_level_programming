d210c5248deb76051f34c8c202dae191b8d29702
Ismaeel Ekundayo <ismaelekundayo3@gmail.com>
2023-03-30 16:18:45 -0700

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalize all string letters
+ * @n: string
+ * Return: whatever
+ */
+
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[i])
+	{
+		while (!(n[i] >= 'a' && n[i] <= 'z'))
+				i++;
+			if (n[i - 1] == ' ' || n[i - 1] == '\t' || n[i - 1] == '\n' || n[i - 1] == ',' || n[i - 1] == ';' || n[i - 1] == '.' || n[i - 1] == '!' || n[i - 1] == '?' || n[i - 1] == '"' || n[i - 1] == '(' || n[i - 1] == ')' || n[i - 1] == '{' || n[i - 1] == '}' || i == 0)
+				n[i] -= 32;
+
+		i++;
+	}
+	return (n);
+}
+
+
