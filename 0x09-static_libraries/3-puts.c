4b7f5dda20e4be340456582bfcb755d913353145
Ismaeel Ekundayo <ismaelekundayo3@gmail.com>
2023-04-10 17:48:44 -0700

    static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * main - Entry point
+ * _puts - function to use puts prototype
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
