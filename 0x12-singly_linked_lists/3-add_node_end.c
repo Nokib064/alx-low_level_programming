514181c0494a6c3f990a635352e59ed90feb4b81
ismaeel <ismaelekundayo3@gmail.com>
2023-04-28 00:22:54 -0700

    add node at end of list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,36 +7,36 @@
  * of a list_t list.
  * @head: head of the linked list.
  * @str: string to store in the list.
- * Return: address of the head.
+ * Return: address of the tail or NULL if failed.
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *temp;
-	size_t nchar;
+	list_t *new_end, *b;
+	size_t a;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	new_end = malloc(sizeof(list_t));
+	if (new_end == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
+	new_end->str = strdup(str);
 
-	for (nchar = 0; str[nchar]; nchar++)
+	for (a = 0; str[a]; a++)
 		;
 
-	new->len = nchar;
-	new->next = NULL;
-	temp = *head;
+	new_end->len = a;
+	new_end->next = NULL;
+	b = *head;
 
-	if (temp == NULL)
+	if (b == NULL)
 	{
-		*head = new;
+		*head = new_end;
 	}
 	else
 	{
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
+		while (b->next != NULL)
+			b = b->next;
+		b->next = new_end;
 	}
 
 	return (*head);
