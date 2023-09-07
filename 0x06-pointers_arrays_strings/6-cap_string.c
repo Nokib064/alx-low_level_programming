f77ffdab6b50b15eb16a9a2d9c00e1398d9032b1
ismaeel <ismaelekundayo3@gmail.com>
2023-09-06 17:11:51 -0700

    capitalize-str

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,26 @@ char *cap_string(char *str)
 {
 	int i;
 
-	if(str[0] >= 'a' && str[0] <= 'z')
+	if (str[0] >= 'a' && str[0] <= 'z')
 	{
 		str[0] -= 32;
 	}
 
-	for(i = 1; str[i] != '\0'; i++)
+	for (i = 1; str[i] != '\0'; i++)
 	{
-		if (str[i] == ',' || str[i] == '\n' || str[i] == '\t' || str[i] == ' ' || str[i] == ';' || str[i] =='.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] =='(' || str[i] == ')' || str[i] == '{' || str[i] =='}')
+		if (str[i] == ',' || str[i] == '\n' || str[i] == '\t' ||
+			str[i] == ' ' || str[i] == ';' || str[i] == '.' || str[i] == '!' ||
+			str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' ||
+			str[i] == '{' || str[i] == '}')
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <='z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				str[i +1] -= 32;
+				str[i + 1] -= 32;
 			}
 
 		}
 	}
-	return str;
+	return (str);
 }
 
 
